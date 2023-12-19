fff971c94522606a6af3b4fd24e927c19ba25101
Yovani Morales <83714102+Yovani-Morales@users.noreply.github.com>
2023-12-18 20:23:16 -0600

    feat: header

--- src/App.js
+++
@@ -1,7 +0,0 @@
-function App() {
-  return (
-    <div className="text-2xl text-blue-600">Hello World!</div>
-  );
-}
-
-export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'roboto': ['Roboto', 'sans-serif']
+      },
+      colors: {
+        'primary-white': 'rgb(231, 233, 234)',
+        'twitter': 'rgb(29, 155, 240)',
+        'dark-gray': 'rgb(113, 118, 123)',
+        'very-dark-gray': 'rgb(47, 51, 54)'
+      }
+    },
   },
   plugins: [],
 }
