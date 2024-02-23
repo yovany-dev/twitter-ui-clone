9da3bd0dce01defeb35cced36aae8d13fc2900ed
Yovani Morales <83714102+Yovani-Morales@users.noreply.github.com>
2024-02-22 23:40:13 -0600

    feat: nav animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import { Header } from "./components/Header/Header";
 import { Main } from "./components/Main/Main";
+import { useState } from "react";
 
 function App() {
+  const [scroll, setScroll] = useState(false);
+
+  window.onscroll = function (){
+    // document.body.scrollTop > 200 || document.documentElement.scrollTop > 200
+    if (window.scrollY > 200) {
+      setScroll(true);
+    } else {
+      setScroll(false);
+    }
+  }
+
   return (
     <div className="content">
-      <Header />
+      <Header opacity={scroll} />
       <Main />
     </div>
   );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,11 +4,16 @@ import { Nav } from './Nav';
 import { Post } from './Post';
 import { Profile } from './Profile';
 
-const Header = () => {
+const Header = ({ opacity }) => {
+  let className = 'w-full h-[52.5px] fixed bottom-0 z-10 border-t border-very-dark-gray bg-black transition-opacity duration-500';
+  if (opacity) {
+    className += ' opacity-40'
+  }
+
   return (
     <header>
       <Head />
-      <section className='w-full h-[52.5px] fixed bottom-0 z-10 border-t border-very-dark-gray bg-black'>
+      <section className={className}>
         <div className='h-full'>
           <nav className='h-full'>
             <ul className='h-full list-none'>
