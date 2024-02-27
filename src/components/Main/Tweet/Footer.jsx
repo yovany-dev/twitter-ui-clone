c5fecb665c56919d592d5dee05f2dc9f50de9a05
Yovani Morales <83714102+Yovani-Morales@users.noreply.github.com>
2024-02-26 20:38:07 -0600

    fix: key prop

--- src/components/Main/Tweet/Footer.jsx
+++ src/components/Main/Tweet/Footer.jsx
@@ -24,7 +24,7 @@ const Footer = ({ comment, retweet, like, views }) => {
 
   return (
     <footer className='mt-3 flex justify-between text-[13px] text-dark-gray'>
-      {actions.map(item => <Action name={item.name} value={item.value} />)}
+      {actions.map((item, index) => <Action key={index} name={item.name} value={item.value} />)}
       <Item name={'Save'} />
       <Item name={'Share'} />
       <div className='hidden 500:flex 500:gap-3'>
