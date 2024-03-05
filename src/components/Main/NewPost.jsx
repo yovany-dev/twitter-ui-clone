dd52410d322f67e3fce0914758a7bc8203280fe9
Yovani Morales <83714102+Yovani-Morales@users.noreply.github.com>
2024-03-05 14:22:59 -0600

    feat: add component new post

---
+++ src/components/Main/NewPost.jsx
@@ -0,0 +1,26 @@
+import { SVG } from "../SVG";
+
+const NewPost = () => {
+  const svgs = ['Media', 'Gif', 'Emoji', 'Location'];
+
+  return (
+    <section className="hidden p-4 500:flex gap-2 border-l border-b border-r border-very-dark-gray">
+      <img className="w-10 h-10 t-rounded" src="https://pbs.twimg.com/profile_images/1736916533920604160/YuNnhwJ1_bigger.jpg" alt="Yovany Morales" />
+      <div className="grow pt-1 flex items-end justify-between">
+        <div className="flex flex-col gap-7">
+          <p className="text-xl text-dark-gray">What is happening?!</p>
+          <div className="mb-2 flex gap-5">
+            {
+              svgs.map((svg, i) => (
+                <SVG key={i} name={svg} w="20px" h="20px" color="#1d9bf0" />
+              ))
+            }
+          </div>
+        </div>
+        <button className="h-9 px-4 font-bold t-rounded bg-twitter opacity-50">Post</button>
+      </div>
+    </section>
+  )
+}
+
+export { NewPost };
