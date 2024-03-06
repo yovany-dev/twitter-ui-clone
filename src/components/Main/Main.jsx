56f9a01d72871dd1b85a1dcfa35fe83e43663498
Yovani Morales <83714102+Yovani-Morales@users.noreply.github.com>
2024-03-05 19:49:46 -0600

    feat: responsive main for screen 688

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -5,8 +5,8 @@ import { Aside } from './Aside';
 
 const Main = () => {
   return (
-    <main className='500:grow 500:w-full mt-[105px] 500:mt-0'>
-      <div className='500:w-full'>
+    <main className='500:grow 688:grow-[2] 500:w-full 688:w-auto mt-[105px] 500:mt-0'>
+      <div className='500:w-full 688:w-[600px]'>
         <Head />
         <NewPost />
         <Content />
