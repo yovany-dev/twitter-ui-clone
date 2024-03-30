377c9cc007b9710564ffa29550d7c31319971bee
Yovani Morales <83714102+Yovani-Morales@users.noreply.github.com>
2024-03-30 17:57:34 -0600

    feat(aside): links footer

--- src/components/Main/Aside/Footer.jsx
+++ src/components/Main/Aside/Footer.jsx
@@ -1,14 +1,13 @@
 import { SVG } from "../../SVG";
 
 const Footer = () => {
+  const links = ['Terms of Service', 'Privacy Policy', 'Cookie Policy', 'Accessibility', 'Ads info'];
+  const linksItems = links.map((item, index) => <a key={index} href={`#${item}`}>{item}</a>);
+
   return (
     <section>
       <nav className="px-4 flex flex-wrap text-[13px] gap-x-3 text-dark-gray">
-        <a href="#">Terms of Service</a>
-        <a href="#">Privacy Policy</a>
-        <a href="#">Cookie Policy</a>
-        <a href="#">Accessibility</a>
-        <a href="#">Ads info</a>
+        {linksItems}
         <div className="flex items-center">
           <p>More</p>
           <SVG name={'ThreePoints'} w="17px" h="13px" color="#71767b" />
