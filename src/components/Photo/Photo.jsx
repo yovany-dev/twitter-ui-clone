3f9be46f67a27d68ed1e4bdee22b2d5069c0b2e1
Yovany Morales <83714102+yovany-dev@users.noreply.github.com>
2024-05-01 21:26:49 -0600

    feat: create photo component

---
+++ src/components/Photo/Photo.jsx
@@ -0,0 +1,9 @@
+const Photo = () => {
+  return (
+    <div className="w-10 h-10 t-rounded">
+      <img className="w-full h-full" src="https://pbs.twimg.com/profile_images/1785799157073969152/c_VRRJui_bigger.jpg" alt="Yovany Morales" />
+    </div>
+  )
+}
+
+export { Photo };
